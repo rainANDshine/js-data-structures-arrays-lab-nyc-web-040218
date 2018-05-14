0c7d00b345e399ad4a138018127e3a587e134879
Shun Yao <ysmiracle@hotmail.com>
2018-05-14 03:21:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ function destructivelyPrependDriver(name) {
   drivers.unshift(name);
 }
 
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  return [...drivers, name];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers];
+}
+
+function removeLastDriver() {
+  return drivers.slice(0, -2);
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1);
+}
