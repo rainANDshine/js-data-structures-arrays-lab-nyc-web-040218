b648425eb0bdf98d63b3a944ecf9691dd1b2a0ee
Shun Yao <ysmiracle@hotmail.com>
2018-05-14 03:14:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
